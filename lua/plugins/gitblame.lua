2f18652c11da435f1f65fe87c61e6c569e0796cb
Aaron <the.sad@niceboy.org>
2025-01-10 16:55:55 +0800

    feat(plugins): update gitblame

---
+++ lua/plugins/gitblame.lua
@@ -0,0 +1,9 @@
+return {
+  'f-person/git-blame.nvim',
+  event = 'VeryLazy',
+  opts = {
+    enabled = true,
+    message_template = '  <author> • <date> • <summary>',
+    date_format = '%Y/%m/%d %H:%M:%S',
+  },
+}
